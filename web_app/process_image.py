4a405e4f41295a0736a4af022fe5640236309c4f
Jasper Matthe <matthejasper@gmail.com>
2021-03-16 20:22:36 -0700

    adjusted colour composition function

--- web_app/process_image.py
+++ web_app/process_image.py
@@ -1,6 +1,14 @@
 import cv2
 import numpy as np
 
+import spotipy
+import os
+from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
+
+auth_manager = SpotifyClientCredentials()
+sp = spotipy.Spotify(auth_manager=auth_manager)
+
 # add color-genre dictionary as a constant here
 COLOUR_GENRE = {
     "black": "Metal",
@@ -36,26 +44,6 @@ def get_playlist(img: np.ndarray):
     return colours_to_playlist(colour_array, playlist_size)
 
 def get_colour_array(img: np.ndarray):
-    # Reshape img array into 1 x number of elements x 3
-    reshaped_img = img.reshape()
-
-    for
-    # iterate (for-loop) over each color of array and classify color by looking at rgb values.
-    d = dict(enumerate(img.flatten(), 1))
-
-    for color in reshaped_img:
-        # classify color
-        color[0] ==sgs
-
-    red_count = 0
-
-    for color in list:
-        if (color[0] > 200 && < 255) {
-            if (color[1] >0 && < 50) {
-                red_coumt = red_count + 1
-            }
-        }
-
     """Converts an image array with rgb values to a dictionary of discrete colors.
 
     Parameters
@@ -68,12 +56,62 @@ def get_colour_array(img: np.ndarray):
     dict[str, float]
         a dictionary of colour keys where their values represent the percent composition seen in the image.
     """
-    red_percentage = 0.1
+    #sample array to be removed later
+    height = 2 # number of rows
+    width = 4
+    img = np.zeros((height,width,3), np.uint8)
+    img[0][0] = np.array([255, 0, 0]) 
+    img[0][1] = np.array([0, 0, 255])
+    img[0][2] = np.array([165, 42, 42]) 
+    img[0][3] = np.array([0, 100, 100])
+    img[1][0] = np.array([255, 0, 0])
+    img[1][1] = np.array([0, 255, 0])
+    img[1][2] = np.array([161, 255, 66])
+    img[1][3] = np.array([209, 28, 0])
+    print(img)
 
-    diction = {
-        "red": red_percentage,
-        "blue": 0.9
+    #HOW TO RESHAPE ANY IMAGE?
+    newimg = img.reshape(8,3)
+
+    print(newimg)
+
+    red_count = 0
+    green_count = 0
+    blue_count = 0 
+
+    for x in newimg:
+        red_colour = x[0]
+        green_colour = x[1]
+        blue_colour = x[2]
+
+        if (red_colour > 150):
+            if (green_colour < 50):
+                if(blue_colour < 50):
+                    red_count += 1
+                    
+        elif (green_colour > 150):
+            if (red_colour < 50):
+                if(blue_colour < 50):
+                    green_count += 1
+        
+        elif (blue_colour > 150):
+            if (red_colour < 50):
+                if(green_colour < 50):
+                    blue_count += 1
+
+    total_colour = red_count + green_count + blue_count
+
+    red_perc = red_count/total_colour
+    green_perc = green_count/total_colour
+    blue_perc = blue_count/total_colour
+
+    colour_dict = {
+        "red_compo": red_perc,
+        "green_compo": green_perc,
+        "blue_compo": blue_perc,
     }
+
+    print(colour_dict)
     return diction
 
 def colours_to_playlist(colour_array: dict[str, float], playlist_size: int):
@@ -92,44 +130,3 @@ def colours_to_playlist(colour_array: dict[str, float], playlist_size: int):
         a playlist representing the color distribution.
     """
     return 0
-
-"""
-# Online Python compiler (interpreter) to run Python online.
-# Write Python 3 code in this online editor and run it.
-import numpy as np
-
-print("Hello world")
-
-height = 2 # number of rows
-width = 4
-img = np.zeros((height,width,3), np.uint8)
-img[0][0] = np.array([0, 0, 0]) #black
-img[0][1] = np.array([0, 0, 255]) #blue
-img[0][2] = np.array([165, 42, 42]) #brown
-img[0][3] = np.array([0, 100, 100]) #cyan
-img[1][0] = np.array([255, 0, 0]) #red
-img[1][1] = np.array([0, 255, 0]) #green
-img[1][2] = np.array([161, 255, 66]) #shade of green
-img[1][3] = np.array([209, 28, 0]) #shade of red
-
-print(img)
-
-newimg = img.reshape(8,3)
-
-print(newimg)
-
-for x in newimg:
-    red_colour = x[0]
-    green_colour = x[1]
-    blue_colour = x[2]
-    
-    red_count = 0
-    green_count = 0
-    blue_count = 0
-    
-    if (red_colour > 150):
-        if (green_colour < 50):
-            if(blue_colour < 50):
-                print(red_count + 1)
-
-"""
