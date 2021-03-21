1e371e6dc79f24dcb5ec38399a593a23ef6c7b4b
Tiffany Matthe <tiffanymatthe@gmail.com>
2021-03-21 13:50:32 -0700

    Replace relative import with absolute.

--- web_app/app.py
+++ web_app/app.py
@@ -1,3 +1,3 @@
 # Entry point for the application.
-from . import app    # For application discovery by the 'flask' command.
-from . import views  # For import side-effects of setting up routes.
+from web_app import app    # For application discovery by the 'flask' command.
+from web_app import views  # For import side-effects of setting up routes.
