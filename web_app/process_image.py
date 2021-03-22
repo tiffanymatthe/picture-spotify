a0a33dcf72576fc7e7a589299af8e9abdeb766f8
Jasper Matthe <matthejasper@gmail.com>
2021-03-21 22:28:20 -0700

    colour pretty much done

--- web_app/process_image.py
+++ web_app/process_image.py
@@ -16,10 +16,7 @@ client = deezer.Client()
 auth_manager = SpotifyClientCredentials()
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-"""
-npimg = np.fromstring(filestr, np.uint8)
-img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-"""
+
 
 # add colour-genre dictionary as a constant here
 COLOUR_GENRE = {
@@ -37,6 +34,7 @@ COLOUR_GENRE = {
     "purple" : "New Age",
 }
 
+"""
 #basic colour dictionary
 basic_colors = {
     "red" : [255, 0, 0],
@@ -52,12 +50,12 @@ basic_colors = {
     "brown" : [165 ,42, 42],
     "purple" : [128, 0, 128],   
 }
-
 # finding distance between any x and dictionary of colours
 def distance_3d(rgb_1, rgb_2):
     return sqrt(pow(rgb_1[0] - rgb_2[0], 2)+
                 pow(rgb_1[1] - rgb_2[1], 2)+
                 pow(rgb_1[2] - rgb_2[2], 2))
+"""
 
 def get_playlist(img: np.ndarray):
     """Converts an image array with rgb values to a playlist
@@ -118,12 +116,11 @@ def get_colour_array(img: np.ndarray):
     print("value of third dimension: ", dx)
     #HOW TO RESHAPE ANY IMAGE?
     newimg = img.reshape(height * width, 3)
-    
-
-    print("reshaped image:", newimg)
+    #print("reshaped image:", newimg)
     
     colour_count_dict = {
         "red": 0,
+        "green" : 0,
         "yellow": 0,
         "blue": 0,
         "black": 0,
@@ -134,6 +131,37 @@ def get_colour_array(img: np.ndarray):
         "brown": 0,
         "purple": 0,
     }
+
+    colour_redirect_to_colour_count_dict = {
+        'black': 'black',
+        'dark green': 'green',
+        'green': 'green',
+        'navy blue': 'blue',
+        'dark blue': 'blue',
+        'dark teal': 'cyan',
+        'blue': 'blue',
+        'teal': 'cyan',
+        'light green': 'green',
+        'light blue': 'blue',
+        'cyan': 'cyan',
+        'sky blue': 'blue',
+        'brown': 'brown', 
+        'dark purple': 'purple',
+        'maroon': 'brown',
+        'red': 'red',
+        'dark red': 'red',
+        'purple': 'purple',
+        'magenta': 'pink',
+        'pink': 'pink',
+        'dark brown': 'brown',
+        'orange': 'orange', 
+        'olive': 'green',
+        'gold': 'yellow',
+        'mustard': 'yellow',
+        'yellow': 'yellow',
+        'lime green': 'green',
+    }
+
     """
     for x in newimg:
         distance_from_red = distance_3d(x, basic_colors["red"])
@@ -160,28 +188,40 @@ def get_colour_array(img: np.ndarray):
             "brown": distance_from_brown,
             "purple": distance_from_purple,
         }
-
+    
         min_colour_dist = min(distance_dict.values())
         min_colour_key_list = [key for key in distance_dict if distance_dict[key] == min_colour_dist]
         min_colour_key = min_colour_key_list[0]
-        
+        colour_count_dict[min_colour_key] += 1
+    """
     for x in newimg:
         r = x[0]
         g = x[1]
         b = x[2]
+
+        (h, s, v) = colorsys.rgb_to_hsv(r, g, b)
+        (r, g, b) = colorsys.hsv_to_rgb(h, 1, v)
+
+        if (r != 0 and g != 0 and b != 0):
+            print("Faulty color!!!!!!!!!!!!!!!!")
+
         match = "[{}, {}, {}]".format(int(r), int(g), int(b))
 
-        for line in open("satfaces.txt"):
-            if line.startswith(match):
-                print("You were thinking of: " + line.split("] ")[1].strip())
-                break
-              
-        
-    colour_count_dict[min_colour_key] += 1
+        if (match == '[255, 255, 255]'):
+            colour_count_dict['white'] += 1
+        else:
+            for line in open("satfaces.txt"):
+                if line.startswith(match):
+                    colour = line.split("] ")[1].strip()
+                    redirected_colour = colour_redirect_to_colour_count_dict[colour]
+                    colour_count_dict[redirected_colour] += 1
+                    break
 
     total_colour = sum(colour_count_dict.values())
+    print("total_colours: ", total_colour)
     perc_colour_dict = {
         "red": colour_count_dict["red"]/total_colour,
+        "green": colour_count_dict["green"]/total_colour,
         "yellow": colour_count_dict["yellow"]/total_colour,
         "blue": colour_count_dict["blue"]/total_colour,
         "black": colour_count_dict["black"]/total_colour,
@@ -194,7 +234,7 @@ def get_colour_array(img: np.ndarray):
     }
     print("colour_count_dict: ", colour_count_dict)
     print("perc_colour_dict: ", perc_colour_dict)
-    """
+
     return 0
 
 
@@ -225,7 +265,7 @@ def colours_to_playlist(colour_array: dict[str, float], playlist_size: int):
     return 0
 
 
-img = cv2.imread('red_1px.png')
+img = cv2.imread('south_africa.png')
 img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 print(type(img))
@@ -236,10 +276,33 @@ print(type(img.shape))
 height, width, _ = img.shape
 
 if ((width * height) > 1000):
-    resized_img = cv2.resize(img, dsize=(54, 140), interpolation=cv2.INTER_CUBIC)
+    biggest_dim = height
+    if (width > height):
+        biggest_dim = width
+    biggest_pixels = 40
+    scale = int(biggest_dim / biggest_pixels)
+    resized_img = cv2.resize(img, dsize=(int(width/scale), int(height/scale)), interpolation=cv2.INTER_CUBIC)
+    cv2.imwrite("test_resizecv2.png", cv2.cvtColor(resized_img, cv2.COLOR_RGB2BGR))
+
     npimage = np.asarray(resized_img)
-    print(resized_img)
+    #print(resized_img)
+    print(npimage.shape)
 else:
     npimage = img
 
 get_colour_array(npimage)
+
+"""
+npimg = np.fromstring(filestr, np.uint8)
+img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
+"""
+
+"""
+colour_count_dict = {}
+
+for line in open("satfaces.txt"):
+    colour = line.split("] ")[1].strip()
+    colour_count_dict[colour] = colour
+
+print(colour_count_dict)
+"""
