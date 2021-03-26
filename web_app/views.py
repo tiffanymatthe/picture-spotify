d1b01be994d4a2fad566174d02dc6c0c0d10a621
Tiffany Matthe <tiffanymatthe@gmail.com>
2021-03-25 21:32:35 -0700

    Integrate python processing with frontend.

--- web_app/views.py
+++ web_app/views.py
@@ -26,43 +26,41 @@ app.config.update(SECRET_KEY=os.environ['SECRET_KEY'])
 def home():
     if request.referrer is not None and "add_playlist_result" in request.referrer:
         [session.pop(key) for key in list(session.keys())]
-    artist_track = {}
+    track_artist = {}
     if request.method == 'POST':
         if request.form.get('save_playlist'):
             if session.get('file_added') is None:
-                return render_template("home.html", success="No file added yet!", error=True, fileAdded=False, artist_track=artist_track)
+                return render_template("home.html", success="No file added yet!", error=True, fileAdded=False, track_artist=track_artist)
             elif request.form.get('playlist_name') is None or str.strip(request.form.get('playlist_name')) == "":
-                return render_template("home.html", success="Playlist name is empty.", artist_track=artist_track, error=True, fileAdded=True)
+                return render_template("home.html", success="Playlist name is empty.", track_artist=track_artist, error=True, fileAdded=True)
             else:
-                session['playlist_name'] = str.strip(request.form.get('playlist_name'))
+                session['playlist_name'] = str.strip(
+                    request.form.get('playlist_name'))
                 return redirect(url_for('auth'))
         # check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             session.pop('file_added', None)
-            return render_template("home.html", success="File not added.", artist_track=artist_track, error=True, fileAdded=False)
+            return render_template("home.html", success="File not added.", track_artist=track_artist, error=True, fileAdded=False)
         file = request.files['file']
-        filetype = type(file) # fileStorage type. Need to convert to color array
+        # fileStorage type. Need to convert to color array
+        filetype = type(file)
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
             flash('No selected file')
             session.pop('file_added', None)
-            return render_template("home.html", success="File not added.", artist_track=artist_track, error=True, fileAdded=False)
+            return render_template("home.html", success="File not added.", track_artist=track_artist, error=True, fileAdded=False)
         if file and allowed_file(file.filename):
             filestr = file.read()
             npimg = np.fromstring(filestr, np.uint8)
-            img = cv2.imdecode(npimg, cv2.IMREAD_UNCHANGED)
-            artist_track = primg.get_playlist(img)
+            img = cv2.imdecode(npimg, cv2.IMREAD_COLOR)
+            track_artist = primg.get_playlist(img)
             session['file_added'] = 'true'
-            # this need to change to sending file to python module and spitting out playlist
-            # filename = secure_filename(file.filename)
-            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            # return redirect(url_for('uploaded_file',
-            # filename=filename))
-            session['artist_track'] = artist_track
-            return render_template("home.html", artist_track=artist_track, fileAdded=True)
-    return render_template("home.html", artist_track=artist_track, fileAdded=False)
+            session['track_artist'] = track_artist
+            return render_template("home.html", track_artist=track_artist, fileAdded=True)
+    return render_template("home.html", track_artist=track_artist, fileAdded=False)
+
 
 @app.route("/about/")
 def about():
@@ -88,6 +86,7 @@ def auth():
 
     return res
 
+
 @app.route('/add_playlist_result')
 def connect():
     error = request.args.get('error')
@@ -96,11 +95,13 @@ def connect():
     if error:
         print("ERROR")
 
-    artist_track = session['artist_track']
+    track_artist = session['track_artist']
     playlist_name = session['playlist_name']
-    return render_template("add_playlist_result.html", artist_track=artist_track, playlist_name=playlist_name)
+    return render_template("add_playlist_result.html", track_artist=track_artist, playlist_name=playlist_name)
+
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
 
 def allowed_file(filename):
     return '.' in filename and \
